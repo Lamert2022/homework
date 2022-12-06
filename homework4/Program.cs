a9a6ace2c2db1515d92a086d73fb58431e847e99
Dmitriy <118912417+Lamert2022@users.noreply.github.com>
2022-12-06 14:57:58 +0300

    задание 4

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход
+ три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/            
+
+int max = int.MinValue;
+for (int i = 0; i < 3; i++)
+{
+   Console.WriteLine($"Введите число {i + 1}:");
+    var str = Console.ReadLine();
+    int num = Convert.ToInt32(str); 
+   if (num > max)  
+      max = num;
+}
+Console.WriteLine($"Максимальное число: {max}");
